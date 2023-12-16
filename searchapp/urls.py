8dc49cbd21765333d9b40e82b92116b03a0744cc
Godzilla660 <tysonleisiano@gmail.com>
2023-12-16 13:58:25 +0300

    update

--- searchapp/urls.py
+++ searchapp/urls.py
@@ -1,6 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name ='home'),
+    path('new-search', views.newsearch, name='new_search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- searchapp/views.py
+++ searchapp/views.py
@@ -1,7 +1,17 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
 
 # Create your views here.
 
 def home(request):
     return render(request, 'base.html')
+
+
+def newsearch(request):
+    search = request.POST.get('search')
+    context = {
+        'search': search
+    }
+    return render(request, 'searchapp/new_search.html', context)
